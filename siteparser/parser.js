f018344a6476593758b7e5a90c20febc8ade7bfc
alexkuznecov <kuzniecov@mail.ru>
2016-05-03 19:45:52 +0300

    Simple methods

---
+++ siteparser/parser.js
@@ -0,0 +1,9 @@
+var request = require("request");
+var cheerio = require("cheerio");
+var mysql = require('mysql');
+
+function updateInformation(callback) {
+    console.log("Hello World");
+}
+
+exports.updateInformation = updateInformation;
